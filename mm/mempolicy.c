9d44c84b4373199eb025de75b837f44651cc92ad
demotools <quhongliang1984@163.com>
2020-10-13 17:30:26 +0800

    give a try

--- mm/mempolicy.c
+++ mm/mempolicy.c
@@ -1631,7 +1631,7 @@ static long kernel_set_pgtlbreplpolicy(int mode, const unsigned long __user *nma
 	int err;
 	struct mm_struct *mm = current->mm;
 	nodemask_t nodes;
-
+	printk("[mitosis] kernel_set_pgtlbreplpolicy: pid = %d\n",current->pid);
 	if (mode) {
 		err = get_nodes(&nodes, nmask, maxnode);
 		if (err)
