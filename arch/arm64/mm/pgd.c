75def3b61faf56931844183d420e25d2a87b7b66
demotools <quhongliang1984@163.com>
2021-07-27 15:34:20 +0800

    Update pgd.c

--- arch/arm64/mm/pgd.c
+++ arch/arm64/mm/pgd.c
@@ -1446,18 +1446,18 @@ int pgtbl_repl_prepare_replication_for_autoconfig(struct mm_struct *mm, struct t
 		printk("===================================== end of a page\n");
 	}
 	printk("%s:%u all: pud_num=%d, pmd_num=%d, pte_num=%d\n", __FUNCTION__, __LINE__, pud_num, pmd_num,pte_num);
-	
+	spin_unlock(&mm->page_table_lock);
+	task_unlock(newtask);
 	if (err) {
 		mm->repl_pgd_enabled = false;
 		printk("PGREPL: DISABLE MITOSIS DUE TO ERROR\n");
 
 	}
-	unsigned int cpu = task_cpu(newtask);
-	printk("[mitosis] newtast cpu =%d.\n",cpu);
-	check_and_switch_context(mm, cpu);
+	// unsigned int cpu = task_cpu(newtask);
+	// printk("[mitosis] newtast cpu =%d.\n",cpu);
+	// check_and_switch_context(mm, cpu);
 
-	spin_unlock(&mm->page_table_lock);
-	task_unlock(newtask);
+	
 	
 	printk("PTREP: Called pgtbl_repl_prepare_replication  done\n");
 	return err;
